c8195b08f37612b05e21ef76dcc06b31c559d509
Nnley <kragelek86@gmail.com>
2024-10-19 04:19:57 +0300

    api updates

---
+++ src/app/api/cart/[id]/route.ts
@@ -0,0 +1,41 @@
+import { updateCartTotalAmount } from '@/lib/update-cart-total-amount'
+import { NextRequest, NextResponse } from 'next/server'
+import { prisma } from '../../../../prisma/prisma-client'
+
+export async function PATCH(req: NextRequest, { params }: { params: { id: string } }) {
+	try {
+		const id = Number(params.id)
+		const data = (await req.json()) as { quantity: number }
+		const token = req.cookies.get('cartToken')?.value
+
+		if (!token) {
+			return NextResponse.json({ message: 'Нет токена' }, { status: 500 })
+		}
+
+		const cartItem = await prisma.cartItem.findFirst({
+			where: {
+				id,
+			},
+		})
+
+		if (!cartItem) {
+			return NextResponse.json({ message: 'Товар не найден' }, { status: 500 })
+		}
+
+		await prisma.cartItem.update({
+			where: {
+				id,
+			},
+			data: {
+				quantity: data.quantity,
+			},
+		})
+
+		const updateUserCart = await updateCartTotalAmount(token)
+
+		return NextResponse.json(updateUserCart)
+	} catch (error) {
+		console.log(error)
+		return NextResponse.json({ message: 'Не удалось обновить корзину' }, { status: 500 })
+	}
+}
--- src/services/cart.ts
+++ src/services/cart.ts
@@ -2,8 +2,14 @@ import { ApiRoutes } from './constants'
 import { CartDTO } from './dto/cart.dto'
 import { axiosInstance } from './instance'
 
-export const fetchCart = async (): Promise<CartDTO> => {
+export const getCart = async (): Promise<CartDTO> => {
 	const { data } = await axiosInstance.get<CartDTO>(ApiRoutes.CART)
+
+	return data
+}
+
+export const updateItemQuantity = async (itemId: number, quantity: number): Promise<CartDTO> => {
+	const { data } = await axiosInstance.patch<CartDTO>(`${ApiRoutes.CART}/${itemId}`, { quantity })
 
 	return data
 }
