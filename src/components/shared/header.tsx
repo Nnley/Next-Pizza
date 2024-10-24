fe4579e3a1c35987287f57b424b19a4318fb9ed5
Nnley <kragelek86@gmail.com>
2024-10-24 14:09:49 +0300

    Refactor header

--- src/components/shared/header.tsx
+++ src/components/shared/header.tsx
@@ -9,10 +9,12 @@ import { Container } from './container'
 import { SearchInput } from './search-input'
 
 interface Props {
+	hasSearch?: boolean
+	hasCart?: boolean
 	className?: string
 }
 
-export const Header: React.FC<Props> = ({ className }) => {
+export const Header: React.FC<Props> = ({ hasSearch = true, hasCart = true, className }) => {
 	return (
 		<header className={cn('border-b', className)}>
 			<Container className='flex items-center justify-between py-8'>
@@ -26,9 +28,11 @@ export const Header: React.FC<Props> = ({ className }) => {
 					</div>
 				</Link>
 
-				<div className='mx-10 flex-1'>
-					<SearchInput />
-				</div>
+				{hasSearch && (
+					<div className='mx-10 flex-1'>
+						<SearchInput />
+					</div>
+				)}
 
 				<div className='flex items-center gap-3'>
 					<Button variant='outline' className='flex items-center gap-1'>
@@ -36,7 +40,7 @@ export const Header: React.FC<Props> = ({ className }) => {
 						Войти
 					</Button>
 
-					<CartButton />
+					{hasCart && <CartButton />}
 				</div>
 			</Container>
 		</header>
