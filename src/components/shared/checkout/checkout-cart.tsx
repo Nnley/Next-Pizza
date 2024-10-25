80757734c33f100b7395ccb2564677c4d5910256
Nnley <kragelek86@gmail.com>
2024-10-25 19:29:04 +0300

    Fix buttons type; fix skeletons

--- src/components/shared/checkout/checkout-cart.tsx
+++ src/components/shared/checkout/checkout-cart.tsx
@@ -19,24 +19,26 @@ export const CheckoutCart = ({ items, removeCartItem, onClickCountButton, loadin
 			<div className='flex flex-col gap-5'>
 				{loading && [...Array(4)].map((_, index) => <CheckoutCartItemSkeleton key={index} />)}
 
-				{items.map(item => (
-					<CheckoutCartItem
-						key={item.id}
-						id={item.id}
-						imageUrl={item.imageUrl}
-						name={item.name}
-						price={item.price}
-						quantity={item.quantity}
-						details={
-							item.pizzaSize && item.pizzaType
-								? getCartItemDetails(item.pizzaType as PizzaType, item.pizzaSize as PizzaSize, item.ingredients)
-								: ''
-						}
-						disabled={item.disabled}
-						onClickRemove={() => removeCartItem(item.id)}
-						onClickCountButton={type => onClickCountButton(item.id, item.quantity, type)}
-					/>
-				))}
+				{!loading &&
+					items.length > 0 &&
+					items.map(item => (
+						<CheckoutCartItem
+							key={item.id}
+							id={item.id}
+							imageUrl={item.imageUrl}
+							name={item.name}
+							price={item.price}
+							quantity={item.quantity}
+							details={
+								item.pizzaSize && item.pizzaType
+									? getCartItemDetails(item.pizzaType as PizzaType, item.pizzaSize as PizzaSize, item.ingredients)
+									: ''
+							}
+							disabled={item.disabled}
+							onClickRemove={() => removeCartItem(item.id)}
+							onClickCountButton={type => onClickCountButton(item.id, item.quantity, type)}
+						/>
+					))}
 			</div>
 		</WhiteBlock>
 	)
