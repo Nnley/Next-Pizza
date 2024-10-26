f3f8b420ab10ff978f678c58b0b8e578ee90087a
Nnley <kragelek86@gmail.com>
2024-10-26 16:50:06 +0300

    Fix api key

--- src/lib/send-email.ts
+++ src/lib/send-email.ts
@@ -1,7 +1,7 @@
 import { Resend } from 'resend'
 
 export const sendEmail = async (to: string, subject: string, template: React.ReactNode) => {
-	const resend = new Resend(process.env.RESEND_API_KEY)
+	const resend = new Resend(process.env.RESEND_API)
 
 	const { data, error } = await resend.emails.send({
 		from: 'Nnley <onboarding@resend.dev>',
