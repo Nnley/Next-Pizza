40dfb9c4e0e7cee5f48266506a6b892506d2e39a
Nnley <kragelek86@gmail.com>
2024-10-26 20:52:11 +0300

    Created callback api route

---
+++ src/app/api/checkout/callback/route.ts
@@ -0,0 +1,57 @@
+import { PaymentCallbackData } from '@/@types/yookassa'
+import { completedPaymentTemplate } from '@/components/shared/email-templates/completed-payment'
+import { sendEmail } from '@/lib/send-email'
+import { CartItemDTO } from '@/services/dto/cart.dto'
+import { OrderStatus } from '@prisma/client'
+import { NextRequest, NextResponse } from 'next/server'
+import { prisma } from '../../../../../prisma/prisma-client'
+
+export async function POST(req: NextRequest) {
+	try {
+		const body = (await req.json()) as PaymentCallbackData
+
+		const order = await prisma.order.findFirst({
+			where: {
+				id: Number(body.object.metadata.order_id),
+			},
+			include: {
+				promotionCode: true,
+			},
+		})
+
+		if (!order) {
+			return NextResponse.json({ error: 'Заказ не найден' }, { status: 500 })
+		}
+
+		const isCompleted = body.object.status === 'succeeded'
+
+		await prisma.order.update({
+			where: {
+				id: order.id,
+			},
+			data: {
+				status: isCompleted ? OrderStatus.COMPLETED : OrderStatus.CANCELED,
+			},
+		})
+
+		let discountPercentage: number | undefined
+
+		if (order.promotionCode) {
+			discountPercentage = order.promotionCode.discountPercentage
+		}
+
+		const items = order?.items as unknown as CartItemDTO[]
+
+		if (isCompleted) {
+			await sendEmail(
+				order.email,
+				'NextPizza | Ваш заказ успешно оплачен',
+				completedPaymentTemplate({ orderId: order.id, items, discountPercentage })
+			)
+		}
+	} catch (e) {
+		console.log('[CHECKOUT CALLBACK]', e)
+
+		return NextResponse.json({ error: 'Что-то пошло не так' }, { status: 500 })
+	}
+}
