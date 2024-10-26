bdaaa1d9c54e68d4d2d451459d48fa59cb6d58f7
Nnley <kragelek86@gmail.com>
2024-10-26 10:54:44 +0300

    Created send email func

---
+++ src/lib/send-email.ts
@@ -0,0 +1,12 @@
+import { Resend } from 'resend'
+
+export const sendEmail = async (to: string, subject: string, template: React.ReactNode) => {
+	const resend = new Resend(process.env.RESEND_API_KEY)
+
+	const { data, error } = await resend.emails.send({
+		from: 'Nnley <onboarding@resend.dev>',
+		to,
+		subject,
+		react: template,
+	})
+}
