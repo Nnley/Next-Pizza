d2e3815dec6dc16606d066a9fe65061d88d52b50
Nnley <kragelek86@gmail.com>
2024-10-26 08:40:37 +0300

    Added phone input to checkout form

--- src/components/shared/checkout/checkout-personal-form.tsx
+++ src/components/shared/checkout/checkout-personal-form.tsx
@@ -1,4 +1,7 @@
+import { Controller, useFormContext } from 'react-hook-form'
+import { ErrorText } from '../error-text'
 import { FormInput } from '../form'
+import { PhoneInput } from '../phone-input'
 import { WhiteBlock } from '../white-block'
 
 interface Props {
@@ -6,13 +9,34 @@ interface Props {
 }
 
 export const CheckoutPersonalForm = ({ className }: Props) => {
+	const { control, setValue } = useFormContext()
+
+	const onClickClear = () => {
+		setValue('phone', '')
+	}
+
 	return (
 		<WhiteBlock title='2. Персональные данные' className={className}>
 			<div className='grid grid-cols-2 gap-5 p-3'>
 				<FormInput name='firstName' placeholder='Имя' />
 				<FormInput name='lastName' placeholder='Фамилия' />
 				<FormInput name='email' placeholder='E-mail' />
-				<FormInput name='phone' placeholder='Телефон' />
+				<Controller
+					control={control}
+					name='phone'
+					render={({ field, fieldState }) => (
+						<div>
+							<PhoneInput
+								className='border-gray-100 py-6 px-4 text-base'
+								placeholder='Телефон'
+								onChange={field.onChange}
+								onClickClear={onClickClear}
+								value={field.value}
+							/>
+							{fieldState.error?.message && <ErrorText text={fieldState.error.message} className='mt-2' />}
+						</div>
+					)}
+				/>
 			</div>
 		</WhiteBlock>
 	)
