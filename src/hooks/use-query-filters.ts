8dca4b72297332d49908a4c9df9a0c9fb651f575
Nnley <kragelek86@gmail.com>
2024-10-26 21:13:34 +0300

    Fix router rerender

--- src/hooks/use-query-filters.ts
+++ src/hooks/use-query-filters.ts
@@ -1,29 +1,34 @@
 import { useRouter } from 'next/navigation'
 import qs from 'qs'
-import { useEffect } from 'react'
+import React from 'react'
 import { Filters } from './use-filters'
 
 export const useQueryFilters = (filters: Filters) => {
+	const isMounted = React.useRef(false)
 	const router = useRouter()
 
 	const { pizzaTypes, sizes, ingredients, priceFrom, priceTo } = filters
 
-	useEffect(() => {
-		const params: any = {
-			pizzaTypes: Array.from(pizzaTypes),
-			sizes: Array.from(sizes),
-			ingredients: Array.from(ingredients),
+	React.useEffect(() => {
+		if (isMounted.current) {
+			const params: any = {
+				pizzaTypes: Array.from(pizzaTypes),
+				sizes: Array.from(sizes),
+				ingredients: Array.from(ingredients),
+			}
+
+			if (priceFrom !== 0 || priceTo !== 1000) {
+				params.priceFrom = priceFrom
+				params.priceTo = priceTo
+			}
+
+			const query = qs.stringify(params, { arrayFormat: 'comma' })
+
+			router.push(`?${query}`, {
+				scroll: false,
+			})
 		}
 
-		if (priceFrom !== 0 || priceTo !== 1000) {
-			params.priceFrom = priceFrom
-			params.priceTo = priceTo
-		}
-
-		const query = qs.stringify(params, { arrayFormat: 'comma' })
-
-		router.push(`?${query}`, {
-			scroll: false,
-		})
+		isMounted.current = true
 	}, [pizzaTypes, sizes, ingredients, priceFrom, priceTo, router])
 }
