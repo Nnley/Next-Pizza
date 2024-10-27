99476368ec31b40943d0e8a43f66ad317afeffee
Nnley <kragelek86@gmail.com>
2024-10-27 14:51:29 +0300

    Created api routes for verify & me

---
+++ src/app/api/auth/me/route.ts
@@ -0,0 +1,29 @@
+import { getUserSession } from '@/lib/get-user-session'
+import { NextResponse } from 'next/server'
+import { prisma } from '../../../../../prisma/prisma-client'
+
+export async function GET() {
+	try {
+		const user = await getUserSession()
+
+		if (!user) {
+			return NextResponse.json({ message: 'Вы не авторизованы' }, { status: 401 })
+		}
+
+		const data = prisma.user.findUnique({
+			where: {
+				id: Number(user.id),
+			},
+			select: {
+				fullName: true,
+				email: true,
+				password: false,
+			},
+		})
+
+		return NextResponse.json(data)
+	} catch (error) {
+		console.error(error)
+		return NextResponse.json({ message: '[USER_GET] Ошибка сервера' }, { status: 500 })
+	}
+}
