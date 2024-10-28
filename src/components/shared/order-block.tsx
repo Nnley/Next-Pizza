3d4b7c3bf6876cdfc1aaf969b589661d47306f01
Nnley <kragelek86@gmail.com>
2024-10-28 14:10:00 +0300

    Added animations

--- src/components/shared/order-block.tsx
+++ src/components/shared/order-block.tsx
@@ -3,8 +3,8 @@
 import { getStatusDetails } from '@/lib/get-status-details'
 import { cn } from '@/lib/utils'
 import { OrderStatus } from '@prisma/client'
-import { ChevronDown, ChevronUp } from 'lucide-react'
-import React from 'react'
+import { ChevronDown } from 'lucide-react'
+import React, { useEffect, useRef } from 'react'
 import { Button } from '../ui'
 import { Title } from './title'
 
@@ -23,7 +23,20 @@ export const OrderBlock: React.FC<React.PropsWithChildren<Props>> = ({
 	children,
 }) => {
 	const [isOpen, setIsOpen] = React.useState(false)
+	const contentRef = useRef<HTMLDivElement>(null)
+	const [contentHeight, setContentHeight] = React.useState(0)
+
 	const { text, className: statusClassName } = getStatusDetails(status)
+
+	const toggleOpen = () => {
+		setIsOpen(!isOpen)
+	}
+
+	useEffect(() => {
+		if (contentRef.current) {
+			setContentHeight(contentRef.current.scrollHeight)
+		}
+	}, [children])
 
 	return (
 		<div className={cn('bg-white rounded-3xl', className)}>
@@ -32,18 +45,26 @@ export const OrderBlock: React.FC<React.PropsWithChildren<Props>> = ({
 					<Title text={title} size='sm' className='font-bold' />
 					<div className='flex items-center gap-8'>
 						<span className={cn('p-2 px-5 rounded-3xl', statusClassName)}>{text}</span>
-						<Button
-							type='button'
-							className='bg-transparent p-0 hover:bg-transparent'
-							onClick={() => setIsOpen(!isOpen)}
-						>
-							{isOpen ? <ChevronUp size={28} color='#9CA3AF' /> : <ChevronDown size={28} color='#9CA3AF' />}
+						<Button type='button' className='bg-transparent p-0 hover:bg-transparent' onClick={toggleOpen}>
+							<ChevronDown
+								size={28}
+								color='#9CA3AF'
+								className={cn('transition-transform duration-300', {
+									'rotate-180': isOpen,
+								})}
+							/>
 						</Button>
 					</div>
 				</div>
 			)}
 
-			{isOpen && <div className={cn('px-5 pb-4', contentClassName)}>{children}</div>}
+			<div
+				ref={contentRef}
+				className={cn('transition-all duration-500 ease-in-out overflow-hidden', contentClassName)}
+				style={{ maxHeight: isOpen ? `${contentHeight}px` : '0px' }}
+			>
+				<div className={cn('px-5 pb-4', contentClassName)}>{children}</div>
+			</div>
 		</div>
 	)
 }
