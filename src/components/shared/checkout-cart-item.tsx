94759245357590bed241f679d5f6f8c0c03bcd54
Nnley <kragelek86@gmail.com>
2024-10-28 12:40:03 +0300

    Updated cart items

--- src/components/shared/checkout-cart-item.tsx
+++ src/components/shared/checkout-cart-item.tsx
@@ -6,6 +6,10 @@ import { CartItemProps } from './cart-item-details/cart-item-details.types'
 interface Props extends CartItemProps {
 	onClickCountButton?: (type: 'plus' | 'minus') => void
 	onClickRemove?: () => void
+	showCountButtons?: boolean
+	showRemoveButton?: boolean
+	leftContentClassName?: string
+	showQuantity?: boolean
 	className?: string
 }
 
@@ -18,7 +22,11 @@ export const CheckoutCartItem: React.FC<Props> = ({
 	disabled,
 	onClickRemove,
 	onClickCountButton,
+	showCountButtons = true,
+	showRemoveButton = true,
+	showQuantity = false,
 	className,
+	leftContentClassName,
 }) => {
 	return (
 		<div className={cn('flex items-center justify-between', { 'opacity-50 pointer-events-none': disabled }, className)}>
@@ -29,11 +37,14 @@ export const CheckoutCartItem: React.FC<Props> = ({
 
 			<CartItemDetails.Price value={price} />
 
-			<div className='flex items-center gap-5 ml-20'>
-				<CartItemDetails.CountButton onClick={onClickCountButton} value={quantity} />
-				<button type='button' onClick={onClickRemove}>
-					<X className='text-gray-400 cursor-pointer hover:text-gray-600' size={20} />
-				</button>
+			<div className={cn('flex items-center gap-5', leftContentClassName)}>
+				{showCountButtons && <CartItemDetails.CountButton onClick={onClickCountButton} value={quantity} />}
+				{showRemoveButton && (
+					<button type='button' onClick={onClickRemove}>
+						<X className='text-gray-400 cursor-pointer hover:text-gray-600' size={20} />
+					</button>
+				)}
+				{showQuantity && <span className='text-gray-400'>({quantity} шт.)</span>}
 			</div>
 		</div>
 	)
