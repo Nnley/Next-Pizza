2d9b50ee0b2d0149d40a56d2172a8e86e86855b3
Nnley <kragelek86@gmail.com>
2024-10-28 14:13:42 +0300

    Added empty state for cart

--- src/components/shared/cart-button.tsx
+++ src/components/shared/cart-button.tsx
@@ -17,16 +17,22 @@ export const CartButton: React.FC<Props> = ({ className }) => {
 	return (
 		<CartDrawer>
 			<Button className={cn('group relative', loading && 'w-28', className)} loading={loading}>
-				<b>{totalAmount} ₽</b>
-				<span className='h-full w-[1px] bg-white/30 mx-3' />
-				<div className='flex items-center gap-1 transition duration-300 group-hover:opacity-0'>
-					<ShoppingCart size={16} className='relative' strokeWidth={2} />
-					<b>{items.length}</b>
-				</div>
-				<ArrowRight
-					size={20}
-					className='absolute right-5 transition duration-300 -translate-x-2 opacity-0 group-hover:opacity-100 group-hover:translate-x-0'
-				/>
+				{items.length > 0 ? (
+					<>
+						<b>{totalAmount} ₽</b>
+						<span className='h-full w-[1px] bg-white/30 mx-3' />
+						<div className='flex items-center gap-1 transition duration-300 group-hover:opacity-0'>
+							<ShoppingCart size={16} className='relative' strokeWidth={2} />
+							<b>{items.length}</b>
+						</div>
+						<ArrowRight
+							size={20}
+							className='absolute right-5 transition duration-300 -translate-x-2 opacity-0 group-hover:opacity-100 group-hover:translate-x-0'
+						/>
+					</>
+				) : (
+					<span className='text-base px-2'>Корзина</span>
+				)}
 			</Button>
 		</CartDrawer>
 	)
